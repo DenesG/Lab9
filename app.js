7373968d352e8cb62376fba2ade7f3d21e74432a
denes <denesgyorgy97@gmail.com>
2022-11-14 19:57:33 -0800

    added app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const { connectDB } = require("./connectDB.js")
+const { populatePokemons } = require("./populatePokemons.js")
+const { getTypes } = require("./getTypes.js")
+const { handleErr } = require("./errorHandler.js")
+const {
+  PokemonBadRequest,
+  PokemonBadRequestMissingID,
+  PokemonBadRequestMissingAfter,
+  PokemonDbError,
+  PokemonNotFoundError,
+  PokemonDuplicateError,
+  PokemonNoSuchRouteError
+} = require("./errors.js")
